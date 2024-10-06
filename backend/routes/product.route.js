77d956424dc97da0446c0fd2a78742eb0576bd2b
Mahdi Azad <mahdiazad@mahdis-laptop.mynetworksettings.com>
2024-10-05 20:11:49 -0400

    protectRoute and adminRoute middlewares

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { getAllProducts } from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router(); 
+
+
+router.get("/", protectRoute, adminRoute, getAllProducts)
+
 
 export default router;
