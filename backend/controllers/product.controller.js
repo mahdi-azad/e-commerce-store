6380d3c6dc8c8f9200ba8096f2b479d894515277
Mahdi Azad <mahdiazad@mahdis-laptop.mynetworksettings.com>
2024-10-05 22:24:31 -0400

    coupon routes

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -89,4 +89,64 @@ export const deleteProduct = async(req,res) => {
     }
 }
 
-// export const getRecommendedProducts = async(req,)
+export const getRecommendedProducts = async(req,res) => {
+    try {
+        const products = await Product.aggregate([
+            {
+                $sample: {size:3}
+            },
+            {
+                $project:{
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    image: 1,
+                    price: 1
+                }
+            }
+        ])
+
+        res.json(products)
+    } catch (error) {
+        console.log("Error in getRecommendedProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message});
+    }
+}
+
+export const getProductsByCategory = async(req,res) => {
+    const {category} = req.params;
+    try {
+        const products = await Product.find({category});
+        res.json(products);
+    } catch (error) {
+        console.log("Error in getProductsByCategory controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message});
+    }
+}
+
+export const toggleFeaturedProduct = async(req,res) => {
+    try {
+        const product = Product.findById(req.params.id);
+        if(product) {
+            product.isFeatured = !product.isFeatured;
+            const updatedProduct = await product.save();
+            await updateFeaturedProductsCache();
+            res.json(updatedProduct);
+        } else {
+            res.status(404).json({message: "Product not found"})
+        }
+    } catch (error) {
+        console.log("Error in toggleFeaturedProduct controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message});
+    }
+}
+
+async function updateFeaturedProductsCache() {
+    try {
+        //lean method is used to return plain javascript objects instead of full mongoose documents. this improves performance
+        const featuredProducts = await Product.find({isFeatured: true}).lean();
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
+    } catch (error) {
+        console.log("error in update cache function")
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 //routes
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 
 import { connectDB } from "./lib/db.js";
@@ -18,8 +20,10 @@ app.use(express.json()); //allows you to parse the body of the request
 app.use(cookieParser());
 
 //authentication
-app.use("/api/auth", authRoutes)
-app.use("/api/products", productRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupon", couponRoutes)
 
 
 app.listen(PORT, () => {
